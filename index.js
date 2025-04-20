614b04dea9cbfb0f9df9e67e93d4d9e0afc838c2
FhriiPhri <fahripadang050908@gmail.com>
2025-04-20 21:02:49 +0700

    repair all code

--- index.js
+++ index.js
@@ -14,8 +14,5 @@ app.get('/', (req, res) => {
 // public routes
 app.use('/api/auth', authRoutes);
 
-// protected routes
-app.use('/api/user', userRoutes);
-
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
