cfee5f7e51e4dc4b6b667a10f313d7dfd3dda98d
FhriiPhri <fahripadang050908@gmail.com>
2025-04-20 20:51:34 +0700

    repair all code

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
-// server.js (atau index.js)
 const express = require('express');
 const dotenv = require('dotenv');
 const authRoutes = require('./routes/auth');
 const userRoutes = require('./routes/user');
-
 
 dotenv.config();
 const app = express();
--- middleware/auth.js
+++ middleware/auth.js
@@ -1,4 +1,3 @@
-// middleware/auth.js
 const jwt = require('jsonwebtoken');
 module.exports = function authenticateToken(req, res, next) {
   // ambil header Authorization: Bearer <token>
