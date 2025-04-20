2f9ea14e49549a8d8310bff30f2c3277212677f0
FhriiPhri <fahripadang050908@gmail.com>
2025-04-20 21:48:58 +0700

    change all

--- index.js
+++ index.js
@@ -1,25 +1,18 @@
 const express = require('express');
-const path = require('path');
+const dotenv = require('dotenv');
+const authRoutes = require('./routes/auth');
 
+dotenv.config();
 const app = express();
-
-// Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Serve static files
-app.use(express.static(path.join(__dirname, 'public')));
+app.get('/', (req, res) => {
+    res.send('API Kas UMKM Aktif 🚀');
+  });  
 
-// Routes
-const authRouter = require('./routes/auth');
-app.use('/', authRouter);
+// public routes
+app.use('/api', authRoutes);
 
-// Handle 404
-app.use((req, res) => {
-  res.status(404).sendFile(path.join(__dirname, 'public', '404.html'));
-});
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
