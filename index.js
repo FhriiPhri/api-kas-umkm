679cfeb2b99acc9d26905a42929aef5a5c47eea1
FhriiPhri <fahripadang050908@gmail.com>
2025-04-22 21:33:09 +0700

    adding users component

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 const db = require('./config/db'); // koneksi DB
 const transactionsRoute = require('./routes/transactionRoutes'); // route transaksi
+const userRoutes = require('./routes/userRoutes'); // route transaksi
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
